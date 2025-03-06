85a72f80abdb479c2a649ce8b33f9c3d559bdf7b
Kat-tech15 <kelvinkatwai@gmail.com>
2025-03-06 13:47:36 +0300

    functions.py

---
+++ functions.py
@@ -0,0 +1,24 @@
+
+
+def addition(num1,num2):
+    result = num1 + num2
+    print("{0} + {1} = {2}".format(num1, num2, result))
+
+
+def subtraction(num1,num2):
+    result = num1 - num2
+    print("{0} - {1} = {2}".format(num1, num2, result))
+
+
+def multiplication(num1,num2):
+    result = num1 * num2
+    print("{0} * {1} = {2}".format(num1, num2, result))
+
+
+
+def division(num1,num2):
+    if num2 == 0:
+        print("cannot divide by a zero")
+    else:
+        result = num1 / num2
+    print("{0} / {1} = {2}".format(num1, num2, result))
