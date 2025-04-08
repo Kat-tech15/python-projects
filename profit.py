b216faa332afc7e24d16c1a337bdcc96f5251117
Kat-tech15 <kelvinkatwai@gmail.com>
2025-04-08 13:06:46 +0300

    profit.py

---
+++ profit.py
@@ -0,0 +1,11 @@
+# maximum profit in stock prices
+
+def maximum_profit(price):
+    prices_set = set(prices)
+    profit = 0
+
+    for i in prices:
+
+      return profit
+prices = [7,1,5,3,6,4]
+print(maximum_profit(prices))
